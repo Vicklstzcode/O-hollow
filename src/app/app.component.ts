0051a6b42f9bf17227013953d2e10ce440234b9b
Vitor Daniel <vitordanielfalcao890@gmail.com>
2025-11-29 20:30:04 -0300

    tela de login rodando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,9 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet], // Importante: isso permite que as páginas (como Login) apareçam
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styles: []
 })
 export class AppComponent {
   title = 'Mysticos';
